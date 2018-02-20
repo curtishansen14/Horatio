97bb4afcfcb4f68b3581a42de6d51a3676ec1c53
Curtis Hansen <curtishansen14@gmail.com>
2018-02-20 08:19:48 -0600

    Roles

---
+++ Horatio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Horatio.MvcApplication" Language="C#" %>
---
+++ Horatio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Horatio.Startup))]
+namespace Horatio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
