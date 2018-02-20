f360d16b16d7385750b7573993f6b75dcf46cf4d
Curtis Hansen <curtishansen14@gmail.com>
2018-02-20 10:13:53 -0600

    models

---
+++ Horatio/Models/ETA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Horatio.Models
+{
+    public class ETA
+    {
+        [Key]
+        public int ETA_ID { get; set; }
+        public string TIME { get; set; }
+
+    }
+}
---
+++ Horatio/Models/Quest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Horatio.Models
+{
+    public class Quest
+    {
+        [Key]
+        public int Quest_ID { get; set; }
+        public string Title { get; set; }
+
+        [MaxLength(509)]
+        public string Description { get; set; }
+
+        [ForeignKey("ETA")]
+        [Display(Name = "Completion Time")]
+        public string Recommended_Completion_Time { get; set; }
+        
+        public decimal Rating { get; set; }
+
+        [Display(Name = "Completed")]
+        public bool isCompleted { get; set; }
+
+        public List<Task> Tasks { get; set; }
+    }
+}
